19f4dde678e976a038dc90321f9d615a1f577438
Dmitry Matora <dmitry.matora@gmail.com>
2025-10-16 00:01:18 +0300

    chore(app): wrap navigator with SafeAreaProvider in App.tsx

--- apps/Prayer/src/app/App.tsx
+++ apps/Prayer/src/app/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 import AppNavigator from './navigation/AppNavigator';
 
-const App = () => {
-  return (
+const App = () => (
+  <SafeAreaProvider>
     <NavigationContainer>
       <AppNavigator />
     </NavigationContainer>
-  );
-};
+  </SafeAreaProvider>
+);
 
 export default App;
