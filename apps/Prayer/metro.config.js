5acd9a36da680e6615ca82698c79e3fb33fe83b9
Dmitry Matora <dmitry.matora@gmail.com>
2025-10-18 16:14:00 +0300

    fix(prayer): align metro resolver with workspace react

--- apps/Prayer/metro.config.js
+++ apps/Prayer/metro.config.js
@@ -1,5 +1,7 @@
 const { withNxMetro } = require('@nx/react-native');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const path = require('path');
 
 const defaultConfig = getDefaultConfig(__dirname);
 const { assetExts, sourceExts } = defaultConfig.resolver;
@@ -18,6 +20,22 @@ const customConfig = {
   resolver: {
     assetExts: assetExts.filter((ext) => ext !== 'svg'),
     sourceExts: [...sourceExts, 'cjs', 'mjs', 'svg'],
+    extraNodeModules: {
+      react: path.resolve(__dirname, '../../node_modules/react'),
+      'react/jsx-runtime': path.resolve(__dirname, '../../node_modules/react/jsx-runtime.js'),
+      'react/jsx-dev-runtime': path.resolve(__dirname, '../../node_modules/react/jsx-dev-runtime.js'),
+      'react-native': path.resolve(__dirname, '../../node_modules/react-native'),
+    },
+    nodeModulesPaths: [
+      path.resolve(__dirname, '../../node_modules'),
+      path.resolve(__dirname, 'node_modules'),
+    ],
+    blockList: exclusionList([
+      new RegExp(`${path.resolve(__dirname, 'node_modules/react').replace(/[/\\\\]/g, '[\\\\/]')}.*`),
+      new RegExp(`${path
+        .resolve(__dirname, 'node_modules/react-native')
+        .replace(/[/\\\\]/g, '[\\\\/]')}.*`),
+    ]),
   },
 };
 
@@ -28,5 +46,5 @@ module.exports = withNxMetro(mergeConfig(defaultConfig, customConfig), {
   // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
   extensions: [],
   // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
-  watchFolders: [],
+  watchFolders: [path.resolve(__dirname, '../../libs')],
 });
