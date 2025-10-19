d05e9ece6823aa835a6ccd3e1f5f8552290e4ef4
Dmitry Matora <dmitry.matora@gmail.com>
2025-10-19 16:46:15 +0300

    feat(prayer): add DrawerNavigator for selecting prayers

---
+++ apps/Prayer/src/app/navigation/PrayerNavigator.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import PrayerScreen from '../screens/PrayerScreen';
+
+export type PrayerDrawerParamList = {
+  'Божественная литургия': { prayerId: 'liturgy' } | undefined;
+  'Вечерня': { prayerId: 'evening' } | undefined;
+};
+
+const Drawer = createDrawerNavigator<PrayerDrawerParamList, 'PrayerDrawer'>();
+
+const PrayerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      id="PrayerDrawer"
+      screenOptions={{
+        headerTitle: 'Молитвослов',
+      }}
+    >
+      <Drawer.Screen
+        name="Божественная литургия"
+        component={PrayerScreen}
+        initialParams={{ prayerId: 'liturgy' }}
+      />
+      <Drawer.Screen
+        name="Вечерня"
+        component={PrayerScreen}
+        initialParams={{ prayerId: 'evening' }}
+      />
+    </Drawer.Navigator>
+  );
+};
+
+export default PrayerNavigator;
